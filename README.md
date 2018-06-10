65244b45d2c2280bea0e77c5c0836a963a5508ef
Mattthew <@mattthew>
2018-06-09 17:32:17 -0700

    Now saves best killing method per site
    Code also refactored

--- README.md
+++ README.md
@@ -13,20 +13,34 @@ To install this bookmarklet:
 
 ## How to use
 
-* tap the bookmarklet
-* if that doesn't remove the wall, tap the "TRY AGAIN" button in the upper-right corner
-* if that doesn't remove the wall, we're out of luck ¯\\\_(ツ)_/¯
-* to revert all changes, refresh the page
+* tap the bookmark to remove the wall
+* if that doesn't work, tap the "TRY AGAIN" button in the upper-right corner
+* if that doesn't, we're out of luck ¯\\\_(ツ)_/¯
+* to revert all changes, simply refresh the page
 
 ## How it works
 
-Website walls usually have two properties:  they fill most of the page, and they don't have much content compared to the useful part of the page.  When you tap the bookmarklet, it first hides any elements that meet that criteria.  Then it removes any code that prevents scrolling the page up and down.  
+### Method 1
 
-When you tap the "TRY AGAIN" button, it temporarily opens the page in a new window, copies the content before the wall appears, strips-out the Javascript that makes the wall appear, then replaces the content of the current window with that de-walled content.
+Website walls usually have two properties:  they fill most of the page, and they don't have much content compared to the useful part of the page.  This method hides any elements that meet that criteria, then removes any code that prevents scrolling the page up and down.  
+
+### Method 2
+
+This method temporarily opens the page within a new window.  It then copies the content of that window before the wall has appeared, but strips-out the Javascript code that makes the wall appear.  Finally, it replaces the content of the current window with that de-walled content and closes the temporary window.
+
+### Trying again
+
+By default for each website, the bookmarklet tries method-1.  When you tap the "TRY AGAIN" button, it tries method-2, and then remembers to use method-2 for the current website.  After that, tapping "TRY AGAIN" again will try method-1, and revert to using method-1 for this website.
 
 ## Sites where this won't work
 
-KillWall won't work on sites that don't load the useful content until you've complied with their wall (e.g. Forbes.com).  It also won't work on pages that redirect to a new URL, unless you tap "TRY AGAIN" before the redirect occurs.
+KillWall bookmarklet can only remove the wall.  Therefore, it won't work on sites that force you to comply with their wall before fully loading the useful content (e.g. Forbes.com).  
+
+Some websites redirect to a new web page that is all-wall and erase the browser history of the original page.  In these cases, method-1 won't work on the all-wall page.  Tap the "TRY AGAIN" button to use method-2 from now on.  Initially, method-2 won't either, since you're now on a different page, but it will work in the future.  Just go back to whatever link brought you to the original page, tap that link again, then quickly tap the KillWall bookmark before the redirect.
+
+## Privacy
+
+This bookmarklet doesn't collect, transmit, nor share any personally identifiable information nor any information about the websites that you visit.  The only data it save is your preferred wall-killing method for each website.  That data is only stored locally on your computer and never transmitted nor shared.  To view or edit this data, use your browser's cookie settings tools.  The bookmarklet code can only run if and when you tap your saved bookmark.  You can delete the bookmark at any time.  
 
 ## Authors
 
--- killwall bookmarklet.js
+++ killwall bookmarklet.js
@@ -1,60 +1,12 @@
 javascript: (function() {
-	if(!document.querySelector('.mattthew_deblock_button')) {
-		/* if they don't exist yet, add deblock buttons to the DOM */
-		/* add the dismiss button */
-		var button = document.createElement('div');
-		button.innerHTML = 'DISMISS&nbsp;ME';
-		var att = document.createAttribute('style');
-		att.value = 'position:fixed; top:10px; right:10px; display:inline-block; padding:4px 8px; border-radius:4px; z-index:999999; color:white; font-family:sans-serif; font-size:14px; box-shadow:0px 4px 4px rgba(0,0,0,0.4), 0px 0px 4px rgba(0,0,0,0.4); cursor:pointer; background-color:red;';
-		button.setAttributeNode(att);
-		att = document.createAttribute('class');
-		att.value = 'mattthew_deblock_button';
-		button.setAttributeNode(att);
-		var bod = document.querySelector('body');
-		button.addEventListener('click', function(){
-			/* remove all deblock buttons from the DOM */
-			var el = document.querySelectorAll('.mattthew_deblock_button');
-			el[0].parentNode.removeChild(el[0]);
-			el[1].parentNode.removeChild(el[1]);
-		});
-		bod.appendChild(button);
-		/* add the try again */
-		button = document.createElement('div');
-		button.innerHTML = '&nbsp;TRY&nbsp;AGAIN&nbsp;';
-		att = document.createAttribute('style');
-		att.value = 'position:fixed; top:54px; right:10px; display:inline-block; padding:4px 8px; border-radius:4px; z-index:999999; color:white; font-family:sans-serif; font-size:14px; box-shadow:0px 4px 4px rgba(0,0,0,0.4), 0px 0px 4px rgba(0,0,0,0.4); cursor:pointer; background-color:blue;';
-		button.setAttributeNode(att);
-		att = document.createAttribute('class');
-		att.value = 'mattthew_deblock_button';
-		button.setAttributeNode(att);
-		var bod = document.querySelector('body');
-		button.addEventListener("click", function(){
-			/* second attempt reloads the page without script tag elements */
-			/* open this URL in a new window */
-			var w = window.open(location.href,'_blank');
-			w.addEventListener('DOMContentLoaded', function(){
-				/* as soon as that page loads... */
-				var html = w.document.querySelector('html').cloneNode(true);
-				var els = html.querySelectorAll('script');
-				/* strip out all script tag elements before they change the DOM */
-				for(var i=0, il=els.length; i<il; i++) {
-					els[i].parentNode.removeChild(els[i]);
-				}
-				var html = html.innerHTML;
-				/* copy and paste that page's DOM to this page, then close it */
-				document.querySelector('html').innerHTML = html;
-				w.close();
-			}, false);
-		});
-		bod.appendChild(button);
-		/* first attempt: remove any elements that fill */
-		/* the entire screen yet don't contain much content */
+	function method1() {
+		/* remove any elements that fill the entire screen yet don't contain much content */
 		var els = document.querySelectorAll('body *');
 		var wW = window.innerWidth * 0.8;
 		var wH = window.innerHeight * 0.8;
 		var bigs = [];
 		/* tag DOM with first_try marker */
-		bod = document.querySelector('body');
+		var bod = document.querySelector('body');
 		bod.style = 'overflow:auto!important';
 		att = document.createAttribute('class');
 		att.value = 'deblock_first_try';
@@ -77,12 +29,95 @@ javascript: (function() {
 			return b.innerHTML.length - a.innerHTML.length;
 		});
 		var p = 0.5;
-		/* hide any elements with (p) less content than */
-		/* the element with the most content */
+		/* hide any elements with (p) less content than the element with the most content */
 		for (var i = 1, il = bigs.length; i < il; i++) {
 			if(bigs[i].innerHTML.length < bigs[0].innerHTML.length * p) {
 				bigs[i].style = 'display:none!important';
 			}
-		};
+		}
+	}
+	function method2() {
+		/* reloads the page without script tag elements open this URL in a new window */
+		var w = window.open(location.href,'_blank');
+		w.addEventListener('DOMContentLoaded', function(){
+			/* as soon as that page loads... */
+			var html = w.document.querySelector('html').cloneNode(true);
+			var els = html.querySelectorAll('script');
+			/* strip out all script tag elements before they change the DOM */
+			for(var i=0, il=els.length; i<il; i++) {
+				els[i].parentNode.removeChild(els[i]);
+			}
+			var html = html.innerHTML;
+			/* copy and paste that page's DOM to this page, then close it */
+			document.querySelector('html').innerHTML = html;
+			addButtons();
+			w.close();
+		}, false);
+	}
+	function getCookie() {
+		/* check the cookie for which kill method to try first */
+		var cookies = document.cookie.split(';');
+		for(var i=0, il = cookies.length; i<il; i++) {
+			var c = cookies[i];
+			if (c.indexOf('mattthew_deblock=method2first') > -1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	function addButtons() {
+		/* add the dismiss button */
+		var button = document.createElement('div');
+		button.innerHTML = 'DISMISS&nbsp;ME';
+		var att = document.createAttribute('style');
+		att.value = 'position:fixed; top:10px; right:10px; display:inline-block; padding:4px 8px; border-radius:4px; z-index:999999; color:white; font-family:sans-serif; font-size:14px; box-shadow:0px 4px 4px rgba(0,0,0,0.4), 0px 0px 4px rgba(0,0,0,0.4); cursor:pointer; background-color:red;';
+		button.setAttributeNode(att);
+		att = document.createAttribute('class');
+		att.value = 'mattthew_deblock_button';
+		button.setAttributeNode(att);
+		var bod = document.querySelector('body');
+		button.addEventListener('click', function(){
+			/* remove all deblock buttons from the DOM */
+			var el = document.querySelectorAll('.mattthew_deblock_button');
+			el[0].parentNode.removeChild(el[0]);
+			el[1].parentNode.removeChild(el[1]);
+		});
+		bod.appendChild(button);
+		/* add the try again button */
+		button = document.createElement('div');
+		button.innerHTML = '&nbsp;TRY&nbsp;AGAIN&nbsp;';
+		att = document.createAttribute('style');
+		att.value = 'position:fixed; top:54px; right:10px; display:inline-block; padding:4px 8px; border-radius:4px; z-index:999999; color:white; font-family:sans-serif; font-size:14px; box-shadow:0px 4px 4px rgba(0,0,0,0.4), 0px 0px 4px rgba(0,0,0,0.4); cursor:pointer; background-color:blue;';
+		button.setAttributeNode(att);
+		att = document.createAttribute('class');
+		att.value = 'mattthew_deblock_button';
+		button.setAttributeNode(att);
+		var bod = document.querySelector('body');
+		button.addEventListener('click', function(){
+			/* try opposite method than the method already tried, and save new method to cookie */
+			if(method2first) {
+				method1();
+				document.cookie = 'mattthew_deblock=method1first; expires=' + exdate.toUTCString();
+			} else {
+				method2();
+				document.cookie = 'mattthew_deblock=method2first; expires=' + exdate.toUTCString();
+			}
+		});
+		bod.appendChild(button);
+	}
+	/* call functions */
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + 365);
+	if(!document.querySelector('.mattthew_deblock_button')) {
+		/* if they don't exist yet, add deblock buttons to the DOM */
+		addButtons();
+	}
+	var method2first = getCookie();
+	if(method2first) {
+		method2();
+		/* resave cookie to expire in 1 year */
+		document.cookie = 'mattthew_deblock=method2first; expires=' + exdate.toUTCString();
+	} else {
+		method1();
 	}
 })();
